663611c4a606e2a51401927699b4609b4c4a3067
JohnnyG256 <135672955+JohnnyG256@users.noreply.github.com>
2023-08-25 05:51:14 +0000

    Setup openapi-fetch

---
+++ src/lib/server/bungie-fetch.spec.ts
@@ -0,0 +1,8 @@
+import { client } from './bungie-fetch';
+
+describe('client', () => {
+	it('should successfully make requests to the bungie api', async () => {
+		const response = await client.GET('/Destiny2/Manifest/', {});
+		expect(response.response.ok).toBe(true);
+	});
+});
