ab603ed767c3cca7069321a5631430f050f58186
JohnnyG256 <135672955+JohnnyG256@users.noreply.github.com>
2023-09-09 06:32:45 +0000

    Remove vitest globals

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-		globals: true
+		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
