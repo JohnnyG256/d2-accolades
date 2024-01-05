381b0c259795ea5c02f5487046263a4ae54405d9
JohnnyG256 <135672955+JohnnyG256@users.noreply.github.com>
2024-01-05 03:43:03 +0000

    add nav bar

---
+++ src/routes/[membershipId=int]/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Search from "../Search.svelte";
+
+  export let data;
+</script>
+<nav>
+  <Search />
+  <a href={`/${data.membershipId}`}>Overview</a>
+  <a href={`/${data.membershipId}/seals`}>Seals</a>
+</nav>
+
+<slot />
